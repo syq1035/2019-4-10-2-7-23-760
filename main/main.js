73f8641e7694f2ecb17ca9b78f8658e1df39825e
fireflyq <1035394794@qq.com>
2019-05-21 21:11:50 +0800

    success

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    let distancePrice = 0 ;
+    let distance = inputs.distance;
+    let parkTimePrice = inputs.parkTime * 0.25;
+
+    if(distance <= 2){
+        distancePrice = 6;
+    }else if(inputs.distance > 8){
+        distancePrice = 10.8 + 1.2*(distance-8);
+    }else{
+        distancePrice = 6 + 0.8*(distance-2);
+    }
+    let summary = Math.round(distancePrice + parkTimePrice);
+    
+    return summary;
 };
